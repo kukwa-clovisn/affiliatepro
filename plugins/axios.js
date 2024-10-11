14afe7dafa40aa58102ff441681add9e35fc094f
kukwa clovis <kukwaclovisngong3@gmail.com>
2024-10-11 14:23:01 +0100

    new website stable update

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  axios.defaults.baseURL = "http://localhost:3005/api";
-  // axios.defaults.baseURL = "https://api.gospelfxtrader.com";
+  // axios.defaults.baseURL = "http://localhost:3005/api";
+  axios.defaults.baseURL = "https://api.gospelfxtrader.com";
 });
